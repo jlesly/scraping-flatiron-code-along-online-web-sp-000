3df29567063f35c5a09f8f6876ace4a483d12614
Lesly Juarez <leslyj2@gmail.com>
2020-12-04 22:31:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,19 +9,9 @@ class Scraper
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     binding.pry
   end 
-  
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
-  
-end
+end 
 
-
+Scraper.new.get_page 
+  
+  
 
